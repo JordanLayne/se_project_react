4f07d2587edb2164eb5d7a37ce3c91ae270c72a0
Jordan Layne <jordanmlayne2001@gmail.com>
2023-05-10 12:52:50 -0400

    Project 14

--- src/utils/Constants.jsx
+++ src/utils/Constants.jsx
@@ -1,6 +1,8 @@
 export const apiKey = "fbf7ca117e3ded783d431c40d7303b1c";
 export const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${34.850746}&lon=${-82.398964}&units=imperial&appid=${apiKey} `;
-export const baseUrl = "http://localhost:3001";
+export const baseUrl = process.env.NODE_ENV === 'production' 
+? 'https://api.attireadvisor.chickenkiller.com'
+: 'https://localhost:3001';
 
 
 export const processRes = (res) => {
